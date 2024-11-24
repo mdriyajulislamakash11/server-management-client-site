e27b977e4b037f9092254b7b52b357a1269adb7f
akash ahmed <hafejmohammadakash@gmail..com>
2024-11-25 00:11:12 +0600

    finnised

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,19 @@ function App() {
     console.log(user);
 
     fetch("http://localhost:5000/users", {
-      method: "POST", 
+      method: "POST",
       headers: {
-        'content-type' : 'application/json'
+        "content-type": "application/json",
       },
-      body: JSON.stringify(user)
-      
+      body: JSON.stringify(user),
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("in side response data: ", data);
+        console.log("inside response data: ", data);
+
+        const newUser = [...users, data]
+        setUsers(newUser)
+        form.reset();
       });
   };
 
@@ -47,7 +50,9 @@ function App() {
       <div>
         {users.map((user) => (
           <p key={user.id}>
-            {user.id} {user.name}
+            {user.id}
+            {user.name}
+            {user.email}
           </p>
         ))}
       </div>
